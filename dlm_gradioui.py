89f3d4a278b4dfa0d37b3faa04ac968c99a572c3
Vignesh Thondikulam <176424390+VigneshT24@users.noreply.github.com>
2025-11-05 21:28:26 -0800

    Refactor get_bot_response function
    Refactor get_bot_response to separate thoughts and final answer.

--- dlm_gradioui.py
+++ dlm_gradioui.py
@@ -8,12 +8,27 @@ dlm_bot = DLM("apply", "dlm_knowledge.db")
 
 ansi_escape_pattern = re.compile(r'\x1b\[[0-9;]*m')
 
-def get_bot_response(message, history):
-    temp_f = io.StringIO()
+def get_bot_response(message):
+    full_response = io.StringIO()
+    thought_text = "" # to seperate the COT from the final answer
+    actual_response = "" # to seperate the final answer from the COT
 
-    with redirect_stdout(temp_f):
+    with redirect_stdout(full_response):
         dlm_bot.ask(message, True)
-    clean_response = ansi_escape_pattern.sub('', temp_f.getvalue())
+    clean_response = ansi_escape_pattern.sub('', full_response.getvalue())
+
+    # removing empty lines for easier parsing
+    splitted_response = [line for line in clean_response.splitlines() if line.strip()]
+
+    actual_response = splitted_response[len(splitted_response) - 1]
+
+    for line in splitted_response[:-1]:
+        thought_text += line + " "
+
+    # as of now, just seperated the COT and final answer
+    # need to have the model output the COT inside a openable container
+    print("THOUGHTS:" + str(thought_text))
+    print("RESPONSE:" + actual_response)
 
     return clean_response.strip()
 
